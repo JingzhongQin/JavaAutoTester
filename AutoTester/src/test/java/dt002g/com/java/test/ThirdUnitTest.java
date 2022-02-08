f242477a197bebab5e9c361c888a0df1c257a56b
Jingzhong Qin <jiqi1901@student.miun.se>
2022-02-08 23:11:40 +0100

    Add files

---
+++ AutoTester/src/test/java/dt002g/com/java/test/ThirdUnitTest.java
@@ -0,0 +1,22 @@
+package dt002g.com.java.test;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ThirdUnitTest {
+
+    @Test
+    public void whenThis_ThenThat(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void whenSomething_ThenSomething(){
+        assertTrue(true);
+    }
+
+    @Test
+    public void wheSomethingElse_ThenSomethingElse(){
+        assertTrue(true);
+    }
+}
