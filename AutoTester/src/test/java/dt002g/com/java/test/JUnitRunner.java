5843e8177d4153184307c883a87c9f87b3e092de
Jingzhong Qin <jiqi1901@student.miun.se>
2022-02-11 13:18:04 +0100

    add jgit dependency

--- AutoTester/src/test/java/dt002g/com/java/test/JUnitRunner.java
+++ AutoTester/src/test/java/dt002g/com/java/test/JUnitRunner.java
@@ -15,7 +15,7 @@ import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPacka
 
 
 public class JUnitRunner {
-     private final SummaryGeneratingListener listener = new SummaryGeneratingListener();
+    private final SummaryGeneratingListener listener = new SummaryGeneratingListener();
 
     public static void main(String[] args) throws ClassNotFoundException {
         JUnitRunner jUnitRunner = new JUnitRunner();
@@ -25,19 +25,19 @@ public class JUnitRunner {
         summary.printTo(new PrintWriter(System.out));
 
         /***********************
-        TestSuite mySuite = new ActiveTestSuite();
-        JUnitCore junit = new JUnitCore();
-        TextListener textListener = new TextListener(new RealSystem());
-        junit.addListener(textListener);
+         TestSuite mySuite = new ActiveTestSuite();
+         JUnitCore junit = new JUnitCore();
+         TextListener textListener = new TextListener(new RealSystem());
+         junit.addListener(textListener);
 
-        //Get class by "string"
-        Class<?> tempClass = Class.forName("ThirdUnitTest");
-        System.out.println(tempClass);
-        mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(FirstUnitTest.class), 5));
-        mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(SecondUnitTest.class), 3));
-        mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(tempClass), 3));
-        junit.run(tempClass);
-        ***********/
+         //Get class by "string"
+         Class<?> tempClass = Class.forName("ThirdUnitTest");
+         System.out.println(tempClass);
+         mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(FirstUnitTest.class), 5));
+         mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(SecondUnitTest.class), 3));
+         mySuite.addTest(new RepeatedTest(new JUnit4TestAdapter(tempClass), 3));
+         junit.run(tempClass);
+         ***********/
     }
 
     public void runOne(Class<?> c){
@@ -53,9 +53,8 @@ public class JUnitRunner {
         LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
                 .selectors(selectPackage("dt002g.com.java.test")).filters(includeClassNamePatterns(".*Test")).build();
         Launcher launcher = LauncherFactory.create();
-        TestPlan testPlan = launcher.discover(request);
+        TestPlan testPlan = launcher.discover(request); // Testing
         launcher.registerTestExecutionListeners(listener);
         launcher.execute(request);
     }
-
 }
---
+++ AutoTester/src/test/java/dt002g/com/java/test/RepositoryDownloader.java
@@ -0,0 +1,12 @@
+package dt002g.com.java.test;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.GitCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+public class RepositoryDownloader {
+
+    RepositoryDownloader(){
+
+    }
+}
