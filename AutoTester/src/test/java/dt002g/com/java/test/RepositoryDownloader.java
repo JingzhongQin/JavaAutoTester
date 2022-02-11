4ba1e3812bc5bfedc0c99464a181b73257c728a2
toya1800 <toya1800@student.miun.se>
2022-02-11 13:49:09 +0100

    RepositoryDownloader example which downloads a repo

--- AutoTester/src/test/java/dt002g/com/java/test/RepositoryDownloader.java
+++ AutoTester/src/test/java/dt002g/com/java/test/RepositoryDownloader.java
@@ -4,9 +4,29 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.GitCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
+import java.nio.file.Paths;
+
 public class RepositoryDownloader {
 
-    RepositoryDownloader(){
+    String url; // Github repo link
+    final String CLONE_DIRECTORY_PATH = "/temp/";
 
+    public RepositoryDownloader(String url) {
+        this.url = url;
+    }
+
+    public void cloneRepo() {
+        System.out.println("Cloning "+url);
+
+        try {
+            Git.cloneRepository()
+                    .setURI(url)
+                    .setDirectory(Paths.get(CLONE_DIRECTORY_PATH).toFile())
+                    .call();
+            System.out.println("Completed Cloning");
+        } catch (GitAPIException e) {
+            System.out.println("Exception occurred while cloning repo");
+            e.printStackTrace();
+        }
     }
 }
